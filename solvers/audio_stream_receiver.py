ce3a09fcd8e91402c7f24f343195b4bc6c538294
papersbms <papersbms@gmail.com>
2024-09-12 19:38:14 +0100

    Create audio_stream_receiver.py

---
+++ solvers/audio_stream_receiver.py
@@ -0,0 +1,43 @@
+import socket
+import vosk
+import json
+import pyaudio
+
+model = vosk.Model(model_path="")
+rec = vosk.KaldiRecognizer(model, 44100)
+
+TCP_IP = "0.0.0.0"
+TCP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((TCP_IP, TCP_PORT))
+sock.listen(1)
+
+def receive_and_recognize():
+    print("Waiting connection...")
+    conn, addr = sock.accept()
+    print(f"connected: {addr}")
+
+    try:
+        while True:
+            data = conn.recv(4096)
+            if not data:
+                break
+            if rec.AcceptWaveform(data):
+                result = rec.Result()
+                text = json.loads(result)["text"]
+                print(f"Results: {text}")
+            else:
+                partial = rec.PartialResult()
+                print(f"Partial results: {json.loads(partial)['partial']}")
+    except Exception as e:
+        print(f"Error while receiving data: {e}")
+    finally:
+        conn.close()
+
+try:
+    receive_and_recognize()
+except KeyboardInterrupt:
+    print("Stop receive...")
+finally:
+    sock.close()
